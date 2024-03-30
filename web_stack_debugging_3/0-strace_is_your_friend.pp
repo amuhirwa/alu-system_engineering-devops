cf8ddbd3932dfac4c15e2736dd916e270f1be152
amuhirwa <a.muhirwa@alustudent.com>
2024-03-30 08:56:36 -0700

    Task done

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# debugging apache server
+exec { 'fixing typo in wp-settings.php':
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  }
