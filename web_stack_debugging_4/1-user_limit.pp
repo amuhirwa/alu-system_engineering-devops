9471b7b305ac653d9478d2d3be4d9f7c07748d8f
amuhirwa <144004199+amuhirwa@users.noreply.github.com>
2024-03-31 11:51:46 +0200

    Update 1-user_limit.pp

--- web_stack_debugging_4/1-user_limit.pp
+++ web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,5 @@
 
 exec { 'replace holberton users open files limits':
   path    => '/usr/bin:/usr/sbin:/bin',
-  command => 'sed -i "/s/nofile ./nofile 4096/" /etc/security/limits.conf',
+  command => 'sed -i "s/nofile ./nofile 4096/" /etc/security/limits.conf',
 }
